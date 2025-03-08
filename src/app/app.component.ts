fa99f045406d142d4d52c1a27ecfef7a4063c46f
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-08 15:11:28 +0530

    [Dhani] Add. UC03 event data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgUrl= "th.jpg";
+  url= "https://www.bridgelabz.com/";
   message: string = "Hello from BridgeLabz";
+
+  onClick($event: any){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url, "_blank");
+  }
 }
