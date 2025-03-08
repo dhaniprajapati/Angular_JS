01e92161017755cd7789310680a33c9d7ddeed36
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-08 23:36:27 +0530

    [Dhani] Add. UC05 Valid user name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,14 @@ export class AppComponent {
     console.log("Save button is clicked!",$event);
     window.open(this.url, "_blank");
   }
+
+  onInput($event: any){
+    console.log("Change Event Occurred!",$event.data);
+    const nameRegex= RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.userName= $event.target.value;
+    }else{
+      this.userName= "";
+    }
+  }
 }
