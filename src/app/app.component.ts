bae96ee6fe7b73f5def38874b7c2e6ea42749c89
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-08 23:23:59 +0530

    [Dhani] Add. UC04 Hello from bridgeLabz with user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [FormsModule]
 })
 export class AppComponent {
   imgUrl= "th.jpg";
   url= "https://www.bridgelabz.com/";
-  message: string = "Hello from BridgeLabz";
+  userName: string= "";
 
   onClick($event: any){
     console.log("Save button is clicked!",$event);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(FormsModule)]
+}).catch(err => console.error(err));
